047466f939b1cf730b02d00beb8a931a01a07581
MohammadTashkandi <33515445+MohammadTashkandi@users.noreply.github.com>
2017-12-18 13:24:04 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,135 @@
+
+<footer id="footer">
+<div class="container">
+                <div class="col-md-3 col-sm-6">
+                    <h4>About us</h4>
+
+                    <p>We offer the best eShop experience in class by offering fast and safe trading of products. This site is your best choice for selling and buying because we impose no taxes or any extra payments.</p>
+
+                    <hr>
+
+
+                    <hr class="hidden-md hidden-lg hidden-sm">
+
+                </div>
+                <!-- /.col-md-3 -->
+
+                <div class="col-md-3 col-sm-6">
+
+                    <h4>Go to:</h4>
+
+                        <div class="blog-entries">
+                            <div class="item same-height-row clearfix">
+                                <div class="image same-height-always">
+                                    <a href="#">
+                                        <img class="img-responsive" src="img/home-logo.png" alt="">
+                                    </a>
+                                </div>
+                                <div class="name same-height-always">
+                                    <h5><a href="index.php">Home</a></h5>
+                                </div>
+                            </div>
+
+                            <div class="item same-height-row clearfix">
+                                <div class="image same-height-always">
+                                    <a href="#">
+                                        <img class="img-responsive" src="img/shopping-cart-logo.png" alt="">
+                                    </a>
+                                </div>
+                                <div class="name same-height-always">
+                                    <h5><a href="shop-basket.php">Shopping cart</a></h5>
+                                </div>
+                            </div>
+
+                        <div class="item same-height-row clearfix">
+                            <div class="image same-height-always">
+                                <a href="#">
+                                    <img class="img-responsive" src="img/about-logo.png" alt="">
+                                </a>
+                            </div>
+                            <div class="name same-height-always">
+                                <h5><a href="about.php">About</a></h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr class="hidden-md hidden-lg">
+
+                </div>
+                <!-- /.col-md-3 -->
+
+                <div class="col-md-3 col-sm-6">
+
+                    <h4>Contact</h4>
+
+                    <p><strong>King Saud University.</strong>
+                        <br>Northern Ring Rd. Exit 2
+                        <br>Al Nakhil
+                        <br>12372
+                        <br>Riyadh
+                        <br>
+                        <strong>Saudi Arabia</strong>
+                    </p>
+
+                    <a href="contact.html" class="btn btn-small btn-template-main">Go to contact page</a>
+
+                    <hr class="hidden-md hidden-lg hidden-sm">
+
+                </div>
+                <!-- /.col-md-3 -->
+
+
+
+                <div class="col-md-3 col-sm-6">
+
+                    <h4>Account</h4>
+
+                    <div class="photostream">
+                        <div>
+                            <a href="#">
+                                Sign in
+                            </a>
+                        </div>
+                        <div>
+                            <a href="customer-register.php">
+                              Sign up
+                            </a>
+                        </div>
+
+                        <div>
+                            <a href="#" class="external facebook" data-animate-hover="pulse"><i class="fa fa-facebook"></i></a>
+                            <a href="#" class="external gplus" data-animate-hover="pulse"><i class="fa fa-google-plus"></i></a>
+                            <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
+                            <a href="#" class="email" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.col-md-3 -->
+            </div>
+            <!-- /.container -->
+        </footer>
+        <!-- /#footer -->
+
+        <!-- *** FOOTER END *** -->
+
+        <!-- *** COPYRIGHT ***
+_________________________________________________________ -->
+
+        <div id="copyright">
+            <div class="container">
+                <div class="col-md-12">
+                    <p class="pull-left">&copy; 2017. Project group #59</p>
+                        <p class="pull-right">Template by <a href="https://bootstrapious.com">Bootstrapious</a> & <a href="https://fity.cz">Fity.cz</a>
+                         <!-- Not removing these links is part of the license conditions of the template. Thanks for understanding :) If you want to use the template without the attribution links, you can do so after supporting further themes development at https://bootstrapious.com/donate  -->
+                    </p>
+
+                </div>
+            </div>
+        </div>
+        <!-- /#copyright -->
+
+        <!-- *** COPYRIGHT END *** -->
