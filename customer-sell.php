bde28607646e017854c5a50de7d3d5b54e26bba5
dotSaviour <dotsaviourdev@gmail.com>
2017-12-22 00:34:42 +0300

    Organized code

--- customer-sell.php
+++ customer-sell.php
@@ -66,7 +66,7 @@ if(!isLoggedIn())
                 <div class="col-md-5">
                     <ul class="breadcrumb">
 
-                        <li><a href="index.html">Home</a>
+                        <li><a href="index.php">Home</a>
                         </li>
                         <li>My account</li>
                     </ul>
@@ -88,46 +88,6 @@ if(!isLoggedIn())
 
                     <p class="lead">Change your personal details or your password here.</p>
                     <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-
-                    <div class="box">
-
-                        <div class="heading">
-                            <h3 class="text-uppercase">Change password</h3>
-                        </div>
-
-                        <form>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label for="password_old">Old password</label>
-                                        <input type="password" class="form-control" id="password_old">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label for="password_1">New password</label>
-                                        <input type="password" class="form-control" id="password_1">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label for="password_2">Retype new password</label>
-                                        <input type="password" class="form-control" id="password_2">
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.row -->
-
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-template-main"><i class="fa fa-save"></i> Save new password</button>
-                            </div>
-                        </form>
-
-                    </div>
-                    <!-- /.box -->
-
 
                     <div class="box clearfix">
                         <div class="heading">
@@ -188,35 +148,7 @@ if(!isLoggedIn())
                 <!-- *** RIGHT COLUMN ***
          _________________________________________________________ -->
 
-                <div class="col-md-3">
-                    <!-- *** CUSTOMER MENU ***
-_________________________________________________________ -->
-                    <div class="panel panel-default sidebar-menu">
-
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Customer section</h3>
-                        </div>
-
-                        <div class="panel-body">
-
-                            <ul class="nav nav-pills nav-stacked">
-                                <li class="active">
-                                    <a href="customer-orders.html"><i class="fa fa-list"></i> My orders</a>
-                                </li>
-                                <li>
-                                    <a href="customer-wishlist.html"><i class="fa fa-heart"></i> My wishlist</a>
-                                </li>
-                                <li>
-                                    <a href="customer-account.html"><i class="fa fa-user"></i> My account</a>
-                                </li>
-                                <li>
-                                    <a href="index.html"><i class="fa fa-sign-out"></i> Logout</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>
-                    <!-- /.col-md-3 -->
+                <?php include "utility/customer-panel.php"; ?>
 
                     <!-- *** CUSTOMER MENU END *** -->
                 </div>
