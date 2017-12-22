52d0b3d948a159c4a0f9577cd5cad2a3af151b3c
dotSaviour <dotsaviourdev@gmail.com>
2017-12-22 14:07:30 +0300

    edited shop-detail.php page

--- shop-detail.php
+++ shop-detail.php
@@ -54,15 +54,16 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-7">
-                    <h1>White Blouse Armani</h1>
+                    <h1>Item</h1>
                 </div>
+
                 <div class="col-md-5">
                     <ul class="breadcrumb">
-                        <li><a href="index.html">Home</a>
+                        <li><a href="index.php">Home</a>
                         </li>
-                        <li><a href="shop-category.html">Ladies</a>
+                        <li><a href="shop-category.php">Ladies</a>
                         </li>
-                        <li><a href="shop-category.html">Tops</a>
+                        <li><a href="shop-category.php">Tops</a>
                         </li>
                         <li>White Blouse Armani</li>
                     </ul>
@@ -81,12 +82,7 @@
         _________________________________________________________ -->
 
                 <div class="col-md-9">
-
-                    <p class="lead">Built purse maids cease her ham new seven among and. Pulled coming wooded tended it answer remain me be. So landlord by we unlocked sensible it. Fat cannot use denied excuse son law. Wisdom happen suffer common the appear ham beauty
-                        her had. Or belonging zealously existence as by resources.
-                    </p>
-                    <p class="goToDescription"><a href="#details" class="scroll-to text-uppercase">Scroll to product details, material & care and sizing</a>
-                    </p>
+                    <p class="goToDescription"><a href="#details" class="scroll-to text-uppercase">Scroll to see item's description</a></p>
 
                     <div class="row" id="productMain">
                         <div class="col-sm-6">
@@ -112,25 +108,13 @@
 
                                 <form>
                                     <div class="sizes">
-
-                                        <h3>Available sizes</h3>
-
-                                        <label for="size_s">
-                                            <a href="#">S</a>
-                                            <input type="radio" id="size_s" name="size" value="s" class="size-input">
-                                        </label>
-                                        <label for="size_m">
-                                            <a href="#">M</a>
-                                            <input type="radio" id="size_m" name="size" value="m" class="size-input">
-                                        </label>
-                                        <label for="size_l">
-                                            <a href="#">L</a>
-                                            <input type="radio" id="size_l" name="size" value="l" class="size-input">
-                                        </label>
-
+                                        <h3>Price</h3>
                                     </div>
 
-                                    <p class="price">$124.00</p>
+                                    <?php
+                                        //TODO add price
+                                        echo "<p class=\"price\">$124.00</p>";
+                                    ?>
 
                                     <p class="text-center">
                                         <button type="submit" class="btn btn-template-main"><i class="fa fa-shopping-cart"></i> Add to cart</button>
@@ -140,43 +124,31 @@
 
                                 </form>
                             </div>
-
-                            <div class="row" id="thumbs">
-                                <div class="col-xs-4">
-                                    <a href="img/detailbig1.jpg" class="thumb">
-                                        <img src="img/detailsquare.jpg" alt="" class="img-responsive">
-                                    </a>
-                                </div>
-                                <div class="col-xs-4">
-                                    <a href="img/detailbig2.jpg" class="thumb">
-                                        <img src="img/detailsquare2.jpg" alt="" class="img-responsive">
-                                    </a>
-                                </div>
-                                <div class="col-xs-4">
-                                    <a href="img/detailbig3.jpg" class="thumb">
-                                        <img src="img/detailsquare3.jpg" alt="" class="img-responsive">
-                                    </a>
-                                </div>
-                            </div>
                         </div>
 
                     </div>
 
 
                     <div class="box" id="details">
-                        <p>
-                        <h4>Product details</h4>
-                        <p>White lace top, woven, has a round neck, short sleeves, has knitted lining attached</p>
-                        <h4>Material & care</h4>
-                        <ul>
-                            <li>Polyester</li>
-                            <li>Machine wash</li>
-                        </ul>
-                        <h4>Size & Fit</h4>
-                        <ul>
-                            <li>Regular fit</li>
-                            <li>The model (height 5'8" and chest 33") is wearing a size S</li>
-                        </ul>
+
+                        <?php
+                            //TODO post item details here
+                            echo "<h4>Item Description</h4>
+                        <p>White lace top, woven, has a round neck, short sleeves, has knitted lining attached</p>";
+                        ?>
+
+
+                        <?php
+                        if(isset($_COOKIE['auth']) || isset($_SESSION['auth'])) {
+                            echo "<h4>Contact Details</h4>
+                            <ul>
+                                <li>Mobile Number: 055999933</li>
+                                <li>Email: example_group59@ksu.sa</li>
+                                <li>Shipping: World-Wide shipping</li>
+                            </ul>";
+                        } else {
+                            echo "<div class=\"alert alert-warning\" role=\"alert\">You have to login to see contact details</div>";
+                        } ?>
 
                         <blockquote>
                             <p><em>Define style this season with Armani's new range of trendy tops, crafted with intricate details. Create a chic statement look by teaming this lace number with skinny jeans and pumps.</em>
@@ -194,118 +166,6 @@
                         </p>
                     </div>
 
-                    <div class="row">
-                        <div class="col-md-3 col-sm-6">
-                            <div class="box text-uppercase">
-                                <h3>You may also like these products</h3>
-                            </div>
-                        </div>
-
-                        <div class="col-md-3 col-sm-6">
-                            <div class="product">
-                                <div class="image">
-                                    <a href="#">
-                                        <img src="img/product2.jpg" alt="" class="img-responsive image1">
-                                    </a>
-                                </div>
-                                <div class="text">
-                                    <h3>Fur coat</h3>
-                                    <p class="price">$143</p>
-
-                                </div>
-                            </div>
-                            <!-- /.product -->
-                        </div>
-
-                        <div class="col-md-3 col-sm-6">
-                            <div class="product">
-                                <div class="image">
-                                    <a href="#">
-                                        <img src="img/product3.jpg" alt="" class="img-responsive image1">
-                                    </a>
-                                </div>
-                                <div class="text">
-                                    <h3>Fur coat</h3>
-                                    <p class="price">$143</p>
-                                </div>
-                            </div>
-                            <!-- /.product -->
-                        </div>
-
-                        <div class="col-md-3 col-sm-6">
-                            <div class="product">
-                                <div class="image">
-                                    <a href="#">
-                                        <img src="img/product1.jpg" alt="" class="img-responsive image1">
-                                    </a>
-                                </div>
-                                <div class="text">
-                                    <h3>Fur coat</h3>
-                                    <p class="price">$143</p>
-                                </div>
-                            </div>
-                            <!-- /.product -->
-                        </div>
-
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-3 col-sm-6">
-                            <div class="box text-uppercase">
-                                <h3>Products viewed recently</h3>
-                            </div>
-                        </div>
-
-
-                        <div class="col-md-3 col-sm-6">
-                            <div class="product">
-                                <div class="image">
-                                    <a href="#">
-                                        <img src="img/product3.jpg" alt="" class="img-responsive image1">
-                                    </a>
-                                </div>
-                                <div class="text">
-                                    <h3>Fur coat</h3>
-                                    <p class="price">$143</p>
-                                </div>
-                            </div>
-                            <!-- /.product -->
-                        </div>
-
-                        <div class="col-md-3 col-sm-6">
-                            <div class="product">
-                                <div class="image">
-                                    <a href="#">
-                                        <img src="img/product1.jpg" alt="" class="img-responsive image1">
-                                    </a>
-                                </div>
-                                <div class="text">
-                                    <h3>Fur coat</h3>
-                                    <p class="price">$143</p>
-                                </div>
-                            </div>
-                            <!-- /.product -->
-                        </div>
-
-
-                        <div class="col-md-3 col-sm-6">
-                            <div class="product">
-                                <div class="image">
-                                    <a href="#">
-                                        <img src="img/product2.jpg" alt="" class="img-responsive image1">
-                                    </a>
-                                </div>
-                                <div class="text">
-                                    <h3>Fur coat</h3>
-                                    <p class="price">$143</p>
-
-                                </div>
-                            </div>
-                            <!-- /.product -->
-                        </div>
-
-                    </div>
-
                 </div>
                 <!-- /.col-md-9 -->
 
@@ -315,7 +175,7 @@
                 <!-- *** RIGHT COLUMN ***
       _________________________________________________________ -->
 
-                <?php include "utility/categories.php" ?>
+                <?php include "utility/categories.php"; ?>
 
                 <!-- *** RIGHT COLUMN END *** -->
 
